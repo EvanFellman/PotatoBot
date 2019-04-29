66597a1fda2bc656b732e26f1ffc9942883d07a0
Evan Fellman <evan.fellman@gmail.com>
2019-04-29 16:18:49 -0400

    Finished the pokeman framework

--- pokeman.js
+++ pokeman.js
@@ -1,5 +1,8 @@
 const moneyModule = new (require("./money.js"))();
 const POKEBALL_PRICE = 25;
+let types = {};
+let moves = {};
+let pokemans = {};
 module.exports = class Main{
 	//initializes user
 	init(user, data){
@@ -40,6 +43,9 @@ module.exports = class Main{
 	}
 }
 
+
+
+
 class Type{
 	//void constructor(name: string, weakness: Type)
 	constructor(name, weakness){
@@ -69,10 +75,11 @@ class Move{
 		this.attackFunc = attackFunc;
 	}
 
-	//attack(thisMonster: Pokeman, otherMonster: Pokeman) => {damage: number, myStatusEffect: StatusEffect, theirStatusEffect: StatusEffect, description: String}
+	//attack(thisMonster: Pokeman, otherMonster: Pokeman) => {damage: number, myStatusEffect: StatusEffect, theirStatusEffect: StatusEffect}
 	attack(thisMonster, otherMonster){
 		if(Math.random() < this.accuracy / 100){
 			let a = this.attackFunc(thisMonster, otherMonster);
+			a.description = "";
 			if(this.type.isStrongTo(otherMonster.type)){
 				a.damage *= 2;
 				a.description += ` ${this.name} is super effective!`;
@@ -94,15 +101,54 @@ class Move{
 }
 
 class Pokeman{
-	//void constructor(name: String, type: Type, moves: Move[], baseStats: {healthStat: number, attackStat: number, defenseStat: number, speedStat: number})
-	constructor(name, type, moves, baseStats){
+	//void constructor(name: String, type: Type, moves: Move[], baseStats: {healthStat: number, attackStat: number, defenseStat: number, speedStat: number}, level, uniqueStats)
+	constructor(name, type, moves, baseStats, level=1, uniqueStats=null){
 		this.name = name;
 		this.type = type;
 		this.moves = moves;
 		this.xp = 0;
 		this.baseStats = baseStats;
 		this.status = null;  //StatusEffect
-		this.uniqueStats = {healthStat: Math.round(Math.random() * 100), attackStat: Math.round(Math.random() * 100), defenseStat: Math.round(Math.random() * 100), speedStat: Math.round(Math.random() * 100)};
+		this.health = 0;
+		this.level = level;
+		this.uniqueStats = uniqueStats;
+		if(this.uniqueStats === null){
+			this.uniqueStats = {healthStat: Math.round(Math.random() * 100), attackStat: Math.round(Math.random() * 100), defenseStat: Math.round(Math.random() * 100), speedStat: Math.round(Math.random() * 100)};
+		}
+		this.resetPokeman();
+	}
+
+	//natural calcMaxHealth() returns maxHealth
+	calcMaxHealth(){
+		return ((150 + this.uniqueStats.healthStat) / 200) * this.baseStats.healthStat;
+	}
+
+	//sets health to full and resets status
+	resetPokeman(){
+		this.health = this.calcMaxHealth();
+		this.status = null;
+	}
+
+	printHealthBar(){
+		let out = "┌";
+		const length = 100;
+		for(let i = 0; i < length; i++){
+			out += "─";
+		}
+		out += "┐\n│";
+		let asdf = 0;
+		for(asdf = 0; asdf < length * (this.health / this.calcMaxHealth()); asdf++){
+			out += "█"
+		}
+		for(asdf; asdf < length; asdf++){
+			out += "░";
+		}
+		out += "│\n└";
+		for(let i = 0; i < length; i++){
+			out += "─";
+		}
+		out += "┘";
+		return out;
 	}
 
 	//Move[] getMoves()
@@ -128,27 +174,70 @@ class Pokeman{
 	//This deals also deals damage to the other pokeman
 	//String attack(otherMonster: Pokeman, moveIndex: natural)
 	attack(otherMonster, moveIndex){
-		let a = this.moves[moveIndex].attack(this, otherMonster);
-		a.damage *= (this.baseStats.attackStat + this.uniqueStats.attackStat) / 200;
-		a.damage *= (200 + (200 - (otherMonster.baseStats.defenseStat + otherMonster.uniqueStats.defenseStat))) / 400;
-		otherMonster.healthStat -= a.damage;
-		return `${this.name} used ${this.moves[moveIndex].name}!` + a.description;
+		if(this.status !== null && !this.status.active){
+			return this.status.tick(thisMonster);
+		} else {
+			let a = this.moves[moveIndex].attack(this, otherMonster);
+			a.damage *= ((this.baseStats.attackStat + this.uniqueStats.attackStat) / 200) * Math.round(this.level * 0.5);
+			a.damage *= Math.round((200 + (200 - (otherMonster.baseStats.defenseStat + otherMonster.uniqueStats.defenseStat))) / (400 * Math.round(otherMonster.level * 0.5)));
+			otherMonster.health -= a.damage;
+			let description = "";
+			if(a.myStatusEffect !== null){
+				this.status = a.myStatusEffect;
+				description += ` ${this.name} now is ${this.status.name}.`;
+			}
+			if(a.theirStatusEffect !== null){
+				otherMonster.status = a.theirStatusEffect;
+				description += ` ${otherMonster.name} now is ${this.status.name},`;
+			}
+			let statusString = "";
+			if(this.status !== null){
+				statusString += this.status.tick(this);
+				if(!this.status.inEffect){
+					this.status = null;
+				}
+			}
+			return `${description}${this.name} used ${this.moves[moveIndex].name}!${a.description}${statusString}`;
+		}
 	}
 }
 
 class StatusEffect{
-	//void constructor(name: String, description: String, attackFunc: function(thisMonster) => {damage: number, active: boolean} , type: Type)
-	constructor(name, description, attackFunc, type){
+	//void constructor(name: String, description: String, type: Type, active: boolean, attackFunc: function(thisMonster) => number, freeFunc: function() => boolean)
+	constructor(name, description, type, active, attackFunc, freeFunc){
 		this.name = name;
 		this.description = description;
-		this.attackFunc = attackFunc;
+		this.attackFunc = attackFunc;		//function to determine damage
+		this.active = active
+		this.inEffect = true;
+		this.freeFunc = freeFunc;			//function to determine if status is lifted
 	}
 
-	//{damage: number, active: boolean} tick(thisMonster: Pokeman)
+	//String tick(thisMonster: Pokeman)
 	tick(thisMonster){
-		let a = this.attackFunc(thisMonster);
-		a.damage *= this.type.isStrongTo(thisMonster) ? 2 : 1;
-		a.damage *= this.type.isWeakTo(thisMonster) ? 0.5 : 1;
-		return a;
+		if(this.freeFunc()){
+			this.inEffect = false;
+			return ` ${thisMonster.name} is not longer ${this.description}!`;
+		} else {
+			let damage = this.attackFunc(thisMonster);
+			damage *= this.type.isStrongTo(thisMonster.type) ? 2 : 1;
+			damage *= this.type.isWeakTo(thisMonster.type) ? 0.5 : 1;
+			damage *= (200 + (200 - (thisMonster.baseStats.defenseStat + thisMonster.uniqueStats.defenseStat))) / 400;
+			thisMonster.health -= damage;
+			return damage === 0 ? (this.active ? ` ${thisMonster.name} is ${this.description} and cannot fight!` : ` ${thisMonster.name} is still ${this.description}.`) : ` ${thisMonster.name} lost ${damage} health because they are ${this.description}.`;
+		}
 	}
 }
+
+types["fire"] = new Type("fire", new Type("water", null));
+types["water"] = new Type("water", new Type("grass", null));
+types["grass"] = new Type("grass", new Type("fire", null));
+
+moves["Burn Baby Burn"] = new Move("Burn Baby Burn", 65, types["fire"], (a, b) => {return {damage: Math.round(25 + (Math.random() * 25)), myStatusEffect: null, theirStatusEffect: null }});
+
+pokemans["YAH YEET"] = (level, stats) => new Pokeman("YAH YEET", types["fire"], [moves["Burn Baby Burn"]], {healthStat: 300, attackStat: 50, defenseStat: 50, speedStat: 60}, level, stats);
+
+let a = pokemans["YAH YEET"]();
+let b = pokemans["YAH YEET"]();
+console.log(a.attack(b, 0));
+console.log(b.printHealthBar());
