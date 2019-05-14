393d67f14016b8b3f8ff0aad5be97cfbdb85349b
Ishitakumar <ishitakumardps@gmail.com>
2019-05-14 14:38:52 -0400

    Create horserace.js
    created horserace

---
+++ horserace.js
@@ -0,0 +1,8 @@
+const moneyModule = new (require("./moneyModule.js"))();
+
+let horserace = {};
+init(winningHorse, data) { }
+processMessage(msg,command,horserace){
+	const author = msg.author;
+	if
+}
