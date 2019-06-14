b122ad5d3178a9fbb9f1a69480b6ab825c967f71
Ishitakumar <ishitakumardps@gmail.com>
2019-06-14 10:44:06 -0400

    horserace
    made some changes.

--- horserace.js
+++ horserace.js
@@ -1,14 +1,16 @@
 const moneyModule = new (require("./money.js"))();
 //Make a list of things you need to store in a representation of a game below:
 //	- bet Object ----- I'll start you off
-//	-	
-//	-	
+let bet = {};
+let games = {};// used to store horses there chances of winning and the percentage money they can return.
+let speed = [];
+	-	
 //	-	
 
 //Now think of what representation would best store all of that information. Some examples could be a String, Integer, Object, Array, HashTable, etc.
 
-const horseRace = {};
-const bet ={};
+
+
 
 
 
@@ -29,14 +31,9 @@ module.exports = class Main{
 	//	Meaning that putting bets in a user's account would make this difficult as they could participate in more than one game at once where they would bet differently.
 	//	I would put the bet object in the representation of a horse race game.
 	//	We should have some sort of collection of the horse race games.  We also want to associate each game with the ID of the text channel it is in.
-	init(user,bet){ 
-		bet[user.id].horses = [];
-		bet[user.id].bets = 0;
-	} 
-	init(horse,horseRace){
-		horseRace[horse].speed = [];
-		horseRace[horse].win = 0;
+	init(user,data){ 
 	}
+		
 
 	//I have to insert this so it wont bust
 	//helperCommand
@@ -47,57 +44,53 @@ module.exports = class Main{
 
 	processMessage(msg,command,horseRace){
 		const author = msg.author;
+		let exsists = false;// to check if a game is already there
 
 		//I dont know what this is for.  This feels like an incomplete thought.  Please expand on this and I can help.
-		const speed1 = calculatespeed(horse1);
 
 		//I would format this differently.
 		//Make one if statement checking if the first of command is "horserace" or "hr"
 		//	Then in that if statement put all of the other checks so you dont have to keep checking if the first statement is "hr"
 		//Something else to note is that every element in command is lower case.
 		//  This was done to allow users to not care about the casing of the commands they write since we will always process their statement after making all of the letters lower case.
-		if(command[0] === "horserace" || command[0] === "hr" && command[1] === "create" || command[1] === "c"){
-			
-			//This is a test. I think.
-			//I would check to make sure that there isnt a game going on before you create a new game.
-			//Since it would suck if someone just created a new game and thus deleting all of the bets placed.
-			msg.channel.send(speed1);
-			msg.channel.send("initialising new game. Place your bets");
-			msg.channel.send(";1");
-			
-			
-			// also need to send the table giving out informatione
-		} else if(command[0] === "horserace" || command[0] === "hr" && command[1] === "bets" || command[1] === "b"){
-			//check to make sure there is a horse race that is created to bet on before allowing the player to bet
+		if(command.length === 4 && command[0] === "horserace" || command[0] === "hr" && command[1] === "create" || command[1] === "c"){
 
-			msg.channel.send(";2");
-			let bet = command[2];
-			let horse = command[3];
-			//this.betmoney(bet,author,bet);
-
-			//increaseBalance is not located in this class. It is in moneyModule.
-			this.increaseBalance(usersData,author,(-1)*amount);
-		} else if(command[0] === "horserace" || command[0] ==="hr" && command[1] === "start" || command [1] ==="s"){
-			//make sure there is a horse race that is created and there is at least one bet on it
-
-			// calculate the winning horse
-			msg.channel.send(";3");
-		}		
+			
+			if(exsists = false){
+				msg.channel.send("initialising new game. Place your bets");
+				games[channel.id][author.id] = {};
+				let thisGame = games[channel.id][author.id];
+				exsists = true;
+				// also need to send the table giving out informatione
+				if(command.length === 4 && command[0] === "horserace" || command[0] === "hr" && command[1] === "bets" || command[1] === "b"){
+				
+					msg.channel.send(";2");
+					let amount = command[2];
+					bets[author.id]={bet: Math.abs(parseInt(command[2],horse: Math.abs(parseInt(command[3]}
+				    //i am trying to save the money each player bets and on which horse in this 
+					moneyModule.increaseBalance(usersData,author,(-1)*amount);
+					if(command[0] === "horserace" || command[0] ==="hr" && command[1] ==="start"|| command[1] === "st"){
+						let won = calculateWinningHorse();
+						msg.channel.send("horse"+ i+1 + "won");
+						//here i am trying to print which horse won.The function returns the index in the speed array 
+						//and this prints the corresponding horse.
+					}
+				}  
+			}
+			
+		} 
 	}
 
 	//betmoney(bet,user,amount){
 	  // bet[user.id].bets = amount;
     //}
 
-    //use camelCase please :)
-    //camelCase helps make your function and variable names easier to read at first glance.
-    //please use it
-
-    //use camelCase please :)
-    calculatespeed(horse){
-    	//I personally used a different algorithm but it doesn't really matter
-    	//I make each of the speeds between 1 and 10
-    	horseRace[horse].speed = getRandomInt(0,20);
+    calculateSpeed(){
+    	for(let i = 0 ; i < 5 ; i++){
+    		speed[i] = getRandomInt(0,10);
+    	}
+   
+    	
     }
 
     getRandomInt(min, max) {
@@ -106,25 +99,25 @@ module.exports = class Main{
  		return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 	}
 
-	//use camelCase please :)
-    calculatechancesofwinning(horse, speed){
-    	//calculateHorseSpeed(Horse) does not exist
-    	//I think you mean calculatespeed
-    	//which needs to be camelCase
-    	let speed = calculateHorseSpeed(horse);
+	
+    calculateChancesOfWinning(speed){
+    	
+    	let speed = calculateSpeed();
 		return speed / totalspeed() * 100;
     }
 
-    //use camelCase please :)
-    totalspeed(){
-    	//calculateHorseSpeed(Horse) does not exist
-    	//I think you mean calculatespeed
-    	//which needs to be camelCase
-		return calculateHorseSpeed(horse1) + calculateHorseSpeed(horse2) + calculateHorseSpeed(horse3) + calculateHorseSpeed(horse4) + calculateHorseSpeed(horse5);
+    
+    totalSpeed(){
+    	let sum = 0;
+    	for(let i = 0 ; i < speed.length ; i++){
+    		sum = sum + speed[i];
+    	}
+    	return speed;
+		
 	}
 
-    //use camelCase please :)
-	calcthechance(){
+    
+	calcTheChance(){
 		//I dont understand what this function does.
 		//I dont exactly like how it works. It biases the top too much. But if you like it dont change it.
 		let speed = []//array of all speeds.// sort in ascending order
@@ -143,28 +136,33 @@ module.exports = class Main{
 		// caculating a biased random chance
 	}
 
-    //use camelCase please :)
-	calculatewinninghorse(){
-		//infinite recursion :D wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-		let try1 = calculatewinninghorse();
-		let try2 = calculatewinninghorse();
-		let try3 = calculatewinninghorse();
+	calculateWinningHorse(){
+		let try1 = calcTheChance();
+		let try2 = calcTheChance();
+		let try3 = calcTheChance();
 		let winner = 0;
-		//Use triple equals to compare
-		//Single equals is for setting variables and does not output a boolean value.
-		if(try1 = try2 && try2 = try3){
-			winner = try 1;
-		} else if(try1 = try2){
+
+		if(try1 === try2 && try2 === try3){
+			winner = try1;
+		} else if(try1 === try2){
 			winner =  try1;
-		} else if(try2 = try3){
+		} else if(try2 === try3){
 			winner = try2;
-		} else if(try3 = try1){
+		} else if(try3 === try1){
 			winner =  try3;
 		} else{
-			winner = 0;
+			winner = Math.random(try1,try2,try3);
 		}
-		//This function doesn't change any variables nor output anything.
-		//I think you intended to return winner but I dont want to write it in case I am wrong.
+		for(let i = 0 ; i < speed.length ; i++){
+			if(winner === speed[i]){
+				return i;
+
+			}
+		}
+		
+	}
+	returnBets(bets{}){
+		// need this to see which player placed bet on correct horse and return the money accordingly
 	}
 }
 
